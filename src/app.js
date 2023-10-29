0a6f57c1719500a88d3f25a43952b6dad943696a
Clarah Moyo <148500389+NaPetha@users.noreply.github.com>
2023-10-30 01:03:31 +0200

    Showing current temperature

--- src/app.js
+++ src/app.js
@@ -1,3 +1,16 @@
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
 let apiKey = "a1e4f511878d61eea5debb02ec1c88e8";
-let apiUrl =
-  "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Johannesburg&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayTemperature);
