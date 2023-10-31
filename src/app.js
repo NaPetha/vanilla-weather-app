e5249599e599685a3f45b68c27d2e0ae21efbd54
Clarah Moyo <148500389+NaPetha@users.noreply.github.com>
2023-10-31 21:58:44 +0200

    Moved forecast HTML to JavaScript

--- src/app.js
+++ src/app.js
@@ -20,6 +20,35 @@ function formatDate(timestamp) {
   let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
 }
+
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Mon", "Tue", "Wed"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+        <div class="col-2">
+            <div class="weather-forecast-date">${day}</div>
+                <img
+                    src="http://openweathermap.org/img/wn/10n@2x.png"
+                    alt=""
+                    width="42"
+                  />
+                <div class="weather-forecast-temperatures">
+              <span class="weather-forecast-temperature-max"> 18° </span>
+              <span class="weather-forecast-temperature-min"> 12° </span>
+          </div>      
+        </div>
+  `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+  console.log(forecastHTML);
+}
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -82,3 +111,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Johannesburg");
+displayForecast();
