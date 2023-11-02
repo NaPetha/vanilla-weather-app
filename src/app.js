e313d393d2f3a48fb9c5c1f00ac0aac9443d41ea
Clarah Moyo <148500389+NaPetha@users.noreply.github.com>
2023-11-02 21:06:14 +0200

    Changed CSS and updated daily forecast

--- src/app.js
+++ src/app.js
@@ -21,32 +21,46 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
 function displayForecast(response) {
-  console.log(response.data);
-  let forecastElement = document.querySelector("#forecast");
+  
 
   let forecastHTML = `<div class="row">`;
-  let days = ["Mon", "Tue", "Wed"];
-  days.forEach(function (day) {
+
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
     forecastHTML =
       forecastHTML +
       `
         <div class="col-2">
-            <div class="weather-forecast-date">${day}</div>
-                <img
-                    src="http://openweathermap.org/img/wn/10n@2x.png"
-                    alt=""
-                    width="42"
-                  />
+            <div class="weather-forecast-date">${formatDay(day.time)}</div>
+              <div>  
+                   <img
+                    src="${
+                      day.condition.icon_url
+                    }" class="weather-forecast-icon" />
+              </div>
                 <div class="weather-forecast-temperatures">
-              <span class="weather-forecast-temperature-max"> 18° </span>
-              <span class="weather-forecast-temperature-min"> 12° </span>
+              <span class="weather-forecast-temperature-max"> <strong>${Math.round(
+                day.temperature.maximum
+              )}°</strong> </span>
+              <span class="weather-forecast-temperature-min"> ${Math.round(
+                day.temperature.minimum
+              )}° </span>
           </div>      
         </div>
   `;
+              }
   });
 
   forecastHTML = forecastHTML + `</div>`;
+  let forecastElement = document.querySelector("#forecast");
   forecastElement.innerHTML = forecastHTML;
 }
 
